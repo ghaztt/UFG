95040db0c68d41d84dc03323fa1c36218b23ea98
Alvezz <67349208+ghaztt@users.noreply.github.com>
2023-10-07 20:58:00 -0300

    Add files via upload

---
+++ AED1/LISTA1/AED1-COMETA.cpp
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+int main (){
+ 
+int i,ano,p;
+i=10;
+
+scanf("%d",&ano);
+
+
+while (i<=ano){
+
+i = i +76;
+		
+}
+
+p = i - ano;
+
+printf("%d", p + ano);
+}
+
+
+
+
+
+  
+
+
+
+
+
+
